e9a4a525b5d810cc5f52e112b5a869b1a5ed7190
unknown <you@example.com>
2026-03-06 19:00:46 +0600

    Customer tickets section customization

--- src/Components/CustomerTickets/CustomerTickets.jsx
+++ src/Components/CustomerTickets/CustomerTickets.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import SingleTicket from '../SingleTicket/SingleTicket';
 
-const CustomerTickets = ({allTickets, clickedTickets , setClickedTickets}) => {
-    
+const CustomerTickets = ({ allTickets, clickedTickets, setClickedTickets }) => {
+
     return (
         <div className='grid grid-cols-1 lg:grid-cols-2 gap-3 lg:gap-6'>
             {
-                allTickets.map((t)=><SingleTicket 
-                key={t.id} 
-                t={t} 
-                clickedTickets={clickedTickets}
-                setClickedTickets={setClickedTickets}
-                ></SingleTicket>)
+                allTickets.length === 0 ? (
+                    <div className='min-h-200 w-full bg-cyan-300/30 rounded-4xl p-5 col-span-2 text-center flext items-center pt-95 '>
+                        <h2 className='text-2xl font-bold text-purple-600'>There is no ticket card to execute. 🎟️🎫</h2>
+                    </div> 
+                    
+                ) : (
+                    allTickets.map((t) => <SingleTicket
+                        key={t.id}
+                        t={t}
+                        clickedTickets={clickedTickets}
+                        setClickedTickets={setClickedTickets}
+                    ></SingleTicket>)
+                )
             }
         </div>
     );
